912e3c74a5a7503d90989d5033aa1e046dec8992
Rayncor <sowa332@gmail.com>
2022-08-27 10:09:30 -0700

    functional login but temp solution to token creation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './Login.css';
 
-export default function Login() {
+//GMS we can move the fetch to its own folder later with other route grabbers but for now this is test
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
+
+
+export default function Login({ setToken }) {
+    //GMS creates local states to capture username and password, which are listened to in onChange
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
+
     return (
         <div className="login-wrapper">
             <h1>Please Log In</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
-                    <input type="text" />
+                    <input type="text" onChange={e => setUserName(e.target.value)} />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input type="password" onChange={e => setPassword(e.target.value)} />
                 </label>
                 <div>
                     <button type="submit">Submit</button>
@@ -20,4 +47,9 @@ export default function Login() {
             </form>
         </div>
     )
+};
+
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
 }
