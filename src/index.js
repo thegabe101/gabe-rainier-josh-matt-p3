7b7f6d472f830ede5c9641ce9265bf2b90417065
Rayncor <sowa332@gmail.com>
2022-08-30 11:19:48 -0700

    trying to make api work

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
 import './index.css';
+import config from "./auth_config.json";
 
 ReactDOM.render(
   <Router>
-    <Auth0ProviderWithHistory>
+    <Auth0ProviderWithHistory
+      domain={config.domain}
+      clientId={config.clientId}
+      audience={config.audience}>
       <App />
     </Auth0ProviderWithHistory>
   </Router>,
