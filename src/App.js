e51e3626601f094a541929ac6acf1a6577923b00
RainierDotulong <rainierkd@gmail.com>
2022-09-01 14:33:24 -0700

    WOOP2

--- src/App.js
+++ src/App.js
@@ -1,66 +1,61 @@
 import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NavBar from './components/nav/nav-bar.js'
 import { Footer } from './components'
 import { Home, Profile, Workouts } from './views'
 import CalendarPage from './views/CalendarPage.js'
-import axios from 'axios';
+import axios from 'axios'
 import './App.css'
-import API from './utils/API';
-import { NextUIProvider } from '@nextui-org/react';
-import LoginForm from "./views/LoginForm";
-import SignUpContainer from "../src/utils/SignUpContainer";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import SignUpForm from './views/Signup.js';
-
-
-
+import API from './utils/API'
+import { NextUIProvider } from '@nextui-org/react'
+import LoginForm from './views/LoginForm'
+import SignUpContainer from '../src/utils/SignUpContainer'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import SignUpForm from './views/Signup.js'
 
 function App() {
+	// const [user, setUser] = useState({
+	//   id: 0,
+	//   email: ''
+	// })
 
-  // const [user, setUser] = useState({
-  //   id: 0,
-  //   email: ''
-  // })
+	// const submitLoginHandle = (email, password) => {
+	//   API.login(email, password).then(res => {
+	//     if (!res.ok) {
+	//       setUser({ userId: 0, email: "" });
+	//       // setToken("")
+	//       return;
+	//     }
+	//     return res.json()
+	//   }).then(data => {
+	//     console.log(data)
+	//     setUser({
+	//       id: data.user.id,
+	//       email: data.user.email
+	//     })
+	//     // setToken(data.token)
+	//     // localStorage.setItem("token",data.token)
+	//   })
+	// }
 
-  // const submitLoginHandle = (email, password) => {
-  //   API.login(email, password).then(res => {
-  //     if (!res.ok) {
-  //       setUser({ userId: 0, email: "" });
-  //       // setToken("")
-  //       return;
-  //     }
-  //     return res.json()
-  //   }).then(data => {
-  //     console.log(data)
-  //     setUser({
-  //       id: data.user.id,
-  //       email: data.user.email
-  //     })
-  //     // setToken(data.token)
-  //     // localStorage.setItem("token",data.token)
-  //   })
-  // }
-
-  return (
-    <Router>
-      <div id="app" className="d-flex flex-column h-100">
-        <NavBar />
-        <div className="container flex-grow-1">
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Route path="/signup" component={SignUpContainer} />
-            <Route path="/login" component={LoginForm} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/workouts" component={Workouts} />
-            <Route path='/calendar' component={CalendarPage} />
-          </Switch>
-        </div>
-        <Footer />
-      </div>
-    </Router>
-  )
+	return (
+		<Router>
+			<div id='app' className='d-flex flex-column h-100'>
+				<NavBar />
+				<div className='container flex-grow-1'>
+					<Switch>
+						<Route path='/home' component={Home} />
+						<Route path='/signup' component={SignUpContainer} />
+						<Route path='/login' component={LoginForm} />
+						<Route path='/profile' component={Profile} />
+						<Route path='/workouts' component={Workouts} />
+						<Route path='/calendar' component={CalendarPage} />
+					</Switch>
+				</div>
+				<Footer />
+			</div>
+		</Router>
+	)
 }
 
 export default App
-
--- src/views/CalendarPage.js
+++ src/views/CalendarPage.js
@@ -1,63 +1,143 @@
-import React, { Component } from 'react'
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
+import React, { Fragment, useState, useCallback, useMemo } from 'react'
+import {
+	Calendar,
+	Views,
+	DateLocalizer,
+	dateFnsLocalizer,
+} from 'react-big-calendar'
+import format from 'date-fns/format'
+import getDay from 'date-fns/getDay'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import PropTypes from 'prop-types'
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
 import '../styles/Calendar.css'
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import events from '../utils/events'
+import Popup from '../components/Popup'
 
-const localizer = momentLocalizer(moment)
-const DnDCalendar = withDragAndDrop(Calendar)
+const locales = {
+	'en-US': require('date-fns/locale/en-US'),
+}
+const localizer = dateFnsLocalizer({
+	format,
+	parse,
+	startOfWeek,
+	getDay,
+	locales,
+})
 
-class CalendarPage extends Component {
-	state = {
-		events: [
-			{
-				start: moment().toDate(),
-				end: moment().add(1, 'days').toDate(),
-				title: 'Some title',
-			},
-		],
-	}
+export default function Selectable() {
+	const [myEvents, setEvents] = useState([])
 
-	
-	onEventResize = (data) => {
-		const { start, end } = data
+	const handleSelectSlot = useCallback(
+		({ start, end }) => {
+			//POST route here
+			const title = window.prompt('New Event name')
+			if (title) {
+				setEvents((prev) => [...prev, { start, end, title }])
+			}
+		},
+		[setEvents]
+	)
 
-		this.setState((state) => {
-			state.events[0].start = start
-			state.events[0].end = end
-			return { events: [...state.events] }
-		})
-	}
+	const handleSelectEvent = useCallback(
+		//PUT Route is final
+		//GET route
+		(event) => window.alert(event.title),
+		[]
+	)
 
-	onEventDrop = (data) => {
-		const { start, end } = data
-
-		this.setState((state) => {
-			state.events[0].start = start
-			state.events[0].end = end
-			return { events: [...state.events] }
-		})
-		console.log(data)
-	}
-
-	render() {
-		return (
-			<div className='App'>
-				<DnDCalendar
-					defaultDate={moment().toDate()}
-					defaultView='month'
-					events={this.state.events}
+	const { defaultDate, scrollToTime } = useMemo(
+		() => ({
+			defaultDate: new Date(),
+			scrollToTime: new Date(1970, 1, 1, 6),
+		}),
+		[]
+	)
+	const [buttonPopup, setButtonPopup] = useState(false)
+	return (
+		<Fragment>
+			<div className='height600'>
+				<Calendar
+					defaultDate={defaultDate}
+					defaultView={Views.MONTH}
+					events={myEvents}
 					localizer={localizer}
-					onEventDrop={this.onEventDrop}
-					onEventResize={this.onEventResize}
-					resizable
-					style={{ height: '100vh' }}
+					style={{ height: 900, width: '95%' }}
+					onSelectEvent={handleSelectEvent}
+					onSelectSlot={handleSelectSlot}
+					selectable
+					scrollToTime={scrollToTime}
 				/>
+				<button onClick={() => setButtonPopup(true)}>OpenPopup</button>
 			</div>
-		)
-	}
+			<div>
+				<Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+					<h3>Popup</h3>
+					<p>THIS POPUP IS TRIGGERED BY THING</p>
+					<input type='text' placeholder='WOOP' />
+					<input type='text' placeholder='WOOP' />
+					<input type='text' placeholder='thats the sound of the police' />
+				</Popup>
+			</div>
+		</Fragment>
+	)
 }
 
-export default CalendarPage
+Selectable.propTypes = {
+	localizer: PropTypes.instanceOf(DateLocalizer),
+}
+
+// class CalendarPage extends Component {
+// 	state = {
+// 		events: [
+// 			{
+// 				start: moment().toDate(),
+// 				end: moment().add(1, 'days').toDate(),
+// 				title: 'Some title',
+// 			},
+// 		],
+// 	}
+
+// 	onEventResize = (data) => {
+// 		const { start, end } = data
+
+// 		this.setState((state) => {
+// 			state.events[0].start = start
+// 			state.events[0].end = end
+// 			return { events: [...state.events] }
+// 		})
+// 	}
+
+// 	onEventDrop = (data) => {
+// 		const { start, end } = data
+
+// 		this.setState((state) => {
+// 			state.events[0].start = start
+// 			state.events[0].end = end
+// 			return { events: [...state.events] }
+// 		})
+// 		console.log(data)
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className='App'>
+// 				<DnDCalendar
+// 					defaultDate={moment().toDate()}
+// 					defaultView='month'
+// 					events={this.state.events}
+// 					localizer={localizer}
+// 					onEventDrop={this.onEventDrop}
+// 					onEventResize={this.onEventResize}
+// 					resizable
+// 					style={{ height: '100vh' }}
+// 				/>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// export default CalendarPage
