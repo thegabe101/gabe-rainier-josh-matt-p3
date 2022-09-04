be66724bc9a6732293aeeeeba01e4a6cde33f485
Rayncor <sowa332@gmail.com>
2022-09-04 13:54:02 -0700

    quick checkout again

--- src/views/Roster.js
+++ src/views/Roster.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component, useState, useEffect } from "react";
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
@@ -7,31 +7,120 @@ import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import '../styles/Roster.css'
 
-const Roster = () => {
-    return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                component='img'
-                alt='green iguana'
-                height='140'
-                width='100'
-                image='https://monstar-lab.com/global/wp-content/uploads/sites/11/2019/04/male-placeholder-image.jpeg'
-            />
-            <CardContent>
-                <Typography gutterBottom variant='h5' component='div'>
-                    Lizard
-                </Typography>
-                <Typography variant='body2' color='text.secondary'>
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size='small'>Share</Button>
-                <Button size='small'>Learn More</Button>
-            </CardActions>
-        </Card>
-    )
+export default function Roster() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+    const [q, setQ] = useState("");
+    const [searchParam] = useState(["username"]);
+    const [filterParam, setFilterParam] = useState(["All"]);
+
+    useEffect(() => {
+        fetch(
+            "http://localhost:3001/api/clients"
+        )
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+    }, []);
+
+    const data = Object.values(items);
+    console.log(Object.values(items));
+    console.log(data)
+    console.log(Object)
+
+
+    function search(items) {
+        return items.filter((item) => {
+            if (item.username == filterParam) {
+                return searchParam.some((newItem) => {
+                    return (
+                        item[newItem]
+                            .toString()
+                            .toLowerCase()
+                            .indexOf(q.toLowerCase()) > -1
+                    );
+                });
+            } else if (filterParam == "All") {
+                return searchParam.some((newItem) => {
+                    return (
+                        item[newItem]
+                            .toString()
+                            .toLowerCase()
+                            .indexOf(q.toLowerCase()) > -1
+                    );
+                });
+            }
+        });
+    }
+
+    if (error) {
+        return (
+            <p>
+                {error.message}, if you get this error, the free API I used
+                might have stopped working, but I created a simple example that
+                demonstrate how this works,{" "}
+                <a href="https://codepen.io/Spruce_khalifa/pen/mdXEVKq">
+                    {" "}
+                    check it out{" "}
+                </a>{" "}
+            </p>
+        );
+    } else if (!isLoaded) {
+        return <>loading...</>;
+    } else {
+        return (
+
+            <div className="wrapper">
+                <div className="search-wrapper">
+                    <label htmlFor="search-form">
+                        <input
+                            type="search"
+                            name="search-form"
+                            id="search-form"
+                            className="search-input"
+                            placeholder="Search for..."
+                            value={q}
+                            onChange={(e) => setQ(e.target.value)}
+                        />
+                        <span className="sr-only">Search for lifter here</span>
+                    </label>
+
+                </div>
+                <ul className="card-grid">
+                    {search(data).map((item) => (
+                        <li>
+                            <article className="rosterBody" key={item.id}>
+                                <div className="rosterBody">
+                                    <img className="rosterBox"
+                                        src="https://assets.vogue.com/photos/59132f18dc6868483f55b55a/master/w_2560%2Cc_limit/00-lede-the-rock.jpg"
+                                        alt={item.username}
+                                    />
+                                    <div className="card-content">
+                                        <h2 className="card-name">{item.username}</h2>
+                                        <ol className="card-list">
+                                            <li>
+                                                Email:{" "}
+                                                <span>{item.email}</span>
+                                            </li>
+                                        </ol>
+                                    </div>
+                                </div>
+                            </article>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
-export default Roster
+
