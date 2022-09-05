dbb00111d192d4f255a1421a78006dfe95f06b8e
RainierDotulong <rainierkd@gmail.com>
2022-09-04 19:30:07 -0700

    asdf

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,43 +1,36 @@
-import { React, useState, useEffect, useRef } from 'react';
+import { React, useState, useEffect, useRef } from 'react'
 import '../styles/Profile.css'
-import API from '../utils/API';
-import therock from '../images/therock.jpg'
-import axios from 'axios';
-import { Image } from "cloudinary-react";
+import API from '../utils/API'
+import axios from 'axios'
+import { Image } from 'cloudinary-react'
 
-const URL_PREFIX = 'http://localhost:3001/' || 'http://lifter-backend-build.herokuapp.com/'
+const cloudNameGuy = 'ddkr1ny4l'
+const presetName = 'pyqqyzxb'
+
+const URL_PREFIX =
+	'http://localhost:3001/' || 'http://lifter-backend-build.herokuapp.com/'
 
 const Profile = () => {
+	const [imageSelected, setImageSelected] = useState('')
 
-	const [imageSelected, setImageSelected] = useState("");
-
-	// constructor() {
-	// 	super();
-
-	// 	this.publicId = {
-	// 	  jsonReturnedValue: null
-	// 	}
-	//   }
-
-	// const [publicId, setPublicId] = useState("");
+	const [publicId, setPublicId] = useState('krp7b71h4ectxfzoedhr')
 
 	const [profile, setProfile] = useState({
-		firstName: "",
-		lastName: "",
-		userName: "",
-		phoneNumber: "",
-		profileImage: "",
-		country: "",
-		city: "",
-		status: ""
+		firstName: '',
+		lastName: '',
+		userName: '',
+		phoneNumber: '',
+		profileImage: '',
+		country: '',
+		city: '',
+		status: '',
 	})
 
-	const oneTimeChange = useRef(true);
-
 	useEffect(() => {
-		if (oneTimeChange.current) {
-			axios.get(URL_PREFIX + `api/coaches/${localStorage.getItem('id')}`).then((response) => {
-				//set responses as profile texts 
+		axios
+			.get(URL_PREFIX + `api/coaches/${localStorage.getItem('id')}`)
+			.then((response) => {
+				//set responses as profile texts
 				const firstName = response.data.firstName
 				const lastName = response.data.lastName
 				const userName = response.data.userName
@@ -46,54 +39,67 @@ const Profile = () => {
 				const country = response.data.country
 				const city = response.data.city
 				const status = response.data.status
-				const profileT = { firstName: firstName, lastName: lastName, userName: userName, phoneNumber: phoneNumber, profileImage: profileImage, country: country, city: city, status: status }
+				const profileT = {
+					firstName: firstName,
+					lastName: lastName,
+					userName: userName,
+					phoneNumber: phoneNumber,
+					profileImage: profileImage,
+					country: country,
+					city: city,
+					status: status,
+				}
 				setProfile(profileT)
 				console.log(profile)
 			})
-			oneTimeChange.current = false;
-		}
-	}, []);
-
-	// function getProfileStuff() {
-	// 	axios.get(URL_PREFIX + `api/coaches/${localStorage.getItem('id')}`).then((response) => {
-	// 		console.log(response)
-	// 	})
-	// }
-
-	// getProfileStuff();
-
+	}, [])
+	useEffect(() => {
+		console.log(publicId)
+		changeImage()
+	}, [publicId])
 
 	function handleFormSubmit(e) {
 		e.preventDefault()
 		console.log('submitting form')
-		// console.log(profile)
-		// const formData = new FormData();
-		// formData.append("file", imageSelected);
-		// formData.append("upload_preset", "pyqqyzxb");
-		// axios.post("https://api.cloudinary.com/v1_1/ddkr1ny4l/image/upload", formData).then((response) => {
-		// 	console.log(response);
-		// })
-		API.putCoaches(localStorage.getItem("id"), profile.firstName, profile.lastName, profile.userName, profile.phoneNumber, profile.publicId, profile.country, profile.city, profile.status)
+		uploadImage()
+		API.putCoaches(
+			//add profile image
+			localStorage.getItem('id'),
+			profile.firstName,
+			profile.lastName,
+			profile.userName,
+			profile.phoneNumber,
+			publicId,
+			profile.country,
+			profile.city,
+			profile.status
+		)
 	}
-
 
 	function handleFormChange(e) {
 		setProfile({ ...profile, [e.target.name]: e.target.value })
 	}
 
-	const uploadImage = () => {
-		// console.log(files[0]);
-		const formData = new FormData();
-		formData.append("file", imageSelected);
-		formData.append("upload_preset", "pyqqyzxb");
-		axios.post("https://api.cloudinary.com/v1_1/ddkr1ny4l/image/upload", formData).then((response) => {
-			console.log(response);
-			// setPublicId(response.data.public_id)
-			// setPublicId(this.response.data.public_id);
-			console.log(response.data.public_id);
-		})
+	const changeImage = () => {
+		const imageId = JSON.stringify(publicId)
+		console.log(imageId)
+		// return imageId
 	}
-
+	const uploadImage = async () => {
+		// console.log(files[0]);
+		const formData = new FormData()
+		formData.append('file', imageSelected)
+		formData.append('upload_preset', presetName)
+		axios
+			.post(
+				`https://api.cloudinary.com/v1_1/${cloudNameGuy}/image/upload`,
+				formData
+			)
+			.then((response) => {
+				console.log(response)
+				setPublicId(response.data.public_id)
+			})
+	}
 
 	return (
 		<div className='divBody'>
@@ -101,16 +107,20 @@ const Profile = () => {
 				<div className='col-md-3 border-right'>
 					<div className='d-flex flex-column align-items-center text-center p-3 py-5'>
 						<input
-							type="file"
-							onChange={(event) => { setImageSelected(event.target.files[0]); }}
+							type='file'
+							onChange={(event) => {
+								setImageSelected(event.target.files[0])
+							}}
 							alt='Profile'
-						// className='rounded-circle mt-5'
-						// width='150px'
 						/>
-						<button onClick={uploadImage}>Upload Profile Picture</button>
 						<span className='font-weight-bold'></span>
 						<span className='text-black-50'></span>
-						<Image className='rounded-circle mt-5' width='150px' cloudName="ddkr1ny4l" publicId="etcinvfhabblkw1m3wvb" />
+						<Image
+							className='rounded-circle mt-5'
+							width='150px'
+							cloudName={cloudNameGuy}
+							publicId={publicId}
+						/>
 					</div>
 				</div>
 
@@ -145,7 +155,7 @@ const Profile = () => {
 								<div className='col-md-12'>
 									<label className='labels'>Username</label>
 									<input
-										name="userName"
+										name='userName'
 										type='text'
 										className='form-control'
 										placeholder='Username'
@@ -155,7 +165,7 @@ const Profile = () => {
 								<div className='col-md-12'>
 									<label className='labels'>Phone Number</label>
 									<input
-										name="phoneNumber"
+										name='phoneNumber'
 										type='text'
 										className='form-control'
 										placeholder='Enter Phone Number'
@@ -167,7 +177,7 @@ const Profile = () => {
 								<div className='col-md-12'>
 									<label className='labels'>Country</label>
 									<input
-										name="country"
+										name='country'
 										type='text'
 										className='form-control'
 										placeholder='Country'
@@ -177,7 +187,7 @@ const Profile = () => {
 								<div className='col-md-12'>
 									<label className='labels'>City</label>
 									<input
-										name="city"
+										name='city'
 										type='text'
 										className='form-control'
 										placeholder='State'
@@ -209,7 +219,7 @@ const Profile = () => {
 						<div className='col-md-12'>
 							<label className='labels'>Status</label>
 							<input
-								name="status"
+								name='status'
 								type='text'
 								className='form-control'
 								placeholder='Status'
@@ -226,5 +236,3 @@ const Profile = () => {
 }
 
 export default Profile
-
-
